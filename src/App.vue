f0a813d4fc0b596cd484ba33a1e1d6a632616025
Hodaya Levin <chalamish.hodaya@gmail.com>
2022-09-12 15:49:19 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Products from "./components/Products.vue";
+import AppHeader from "./components/AppHeader.vue";
+</script>
+
+<template>
+  <AppHeader />
+  <Products />
+</template>
+
+<style scoped></style>
+
---
+++ src/components/Products.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Product from './product.vue';
+
+    export default defineComponent({
+    components: { Product }
+})
+</script>
+<template>
+    <Product/>
+    <Product/>
+    <Product/>    
+</template>
+<style scoped>
+.products {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+ @media (max-width: 920px) {  
+  .products .product-card:first-child,
+  .products .product-card:nth-child(2) {
+    flex: 2 46%;
+  }
+}
+</style>
